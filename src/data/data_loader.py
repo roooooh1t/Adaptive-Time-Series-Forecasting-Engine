5005cb5406ce114311d4ff5df0869021cf08d552
Rahul Singh <rahulsingh654932@gmail.com>
2026-08-06 18:16:27 +0530

    Test2

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,3 +1,9 @@
 import kagglehub
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path=".env") # By default reads environment variables from ".env" only
+
+os.environ["KAGGLE_API_TOKEN"] = os.getenv("KAGGLE_API_TOKEN") # Load the kaggle token
 
 kagglehub.login()
