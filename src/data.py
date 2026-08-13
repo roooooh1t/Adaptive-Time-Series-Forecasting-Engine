4b37320add07a26f200aa0a43840f211fc0ebe38
Rahul Singh <rahulsingh654932@gmail.com>
2026-08-13 15:44:23 +0530

    Completed remaining condiitons in data.py and added data-analysis file

--- src/data.py
+++ src/data.py
@@ -1,8 +1,11 @@
-import kagglehub
-from dotenv import load_dotenv
-from pathlib import Path
 import os
+import kagglehub
+from typing import Literal
+from pathlib import Path
+from dotenv import load_dotenv
+
 import pandas as pd
+
 
 class Data():
             
@@ -36,21 +39,70 @@ class Data():
             a. data of that product across all the shops in all states
             b. data of all the products in that shop.
     '''
-    def get_train_data(self, product_id, choice = ['single_prod_day_wise', 'all_prod_of_1_shop', 'one_prod_in_all_shops']):
-        sales = pd.read_csv(f"{self.data_path}/sales_train_validation.csv")
+    def get_train_data(self, product_id, choice1 : Literal['train', 'validate', 'test'] = 'train' ,  choice2  : Literal['single_prod_day_wise', 'all_prod_of_1_shop', 'one_prod_in_all_shops'] = 'single_prod_day_wise'):
+        train_sales = pd.read_csv(f"{self.data_path}/sales_train_validation.csv")
+        test_sales = pd.read_csv(f"{self.data_path}/sales_train_evaluation.csv")
         calendar = pd.read_csv(f"{self.data_path}/calendar.csv")
         sell_prices = pd.read_csv(f"{self.data_path}/sell_prices.csv")
 
-        if choice == 'single_prod_day_wise':
-            # data = sales[sales['id'] == product_id][6 : self.train_val_split]
-            data = sales[sales['id'] == product_id].iloc[: , 6 : ]
+        if choice2 == 'single_prod_day_wise':
+            # Find the week when this products sale begun
+            broken_id = product_id.split('_')
+            item_id = broken_id[0] + '_' + broken_id[1] + '_' + broken_id[2]
+            store_id = broken_id[3] + '_' + broken_id[4]
+            print(item_id, store_id)
+            product_wm_yr_wk = sell_prices.loc[(sell_prices['store_id'] == store_id) & (sell_prices['item_id'] == item_id)]['wm_yr_wk'].iloc[0]
 
-        elif choice == 'all_prod_of_1_shop':
-            pass
+            # Find the day from which it's sale would have begun [Assumption: Sale started the very first day of the week it has a sale_price]
+            start_day = calendar.index[calendar['wm_yr_wk'] == product_wm_yr_wk][0] # The day number = the row number
 
-        elif choice == 'one_prod_in_all_shops':
-            pass
+            # Get the correct data for a single product
+            if choice1 == 'train':
+                data = train_sales[train_sales['id'] == product_id].iloc[ : , start_day + 5 : self.train_val_split + 6] ## + 5 and 6 are present to shift the indices appropriately since the day columns start from the 6th column
+            elif choice1 == 'validation':
+                data = train_sales[train_sales['id'] == product_id].iloc[ : , start_day + 5 : ] ## + 5 is present to shift the indices appropriately since the day columns start from the 6th column
+            elif choice1 == 'test':
+                data = test_sales[test_sales['id'] == product_id].iloc[ : , start_day + 5 : ] ## + 5 is present to shift the indices appropriately since the day columns start from the 6th column
 
+        elif choice2 == 'all_prod_of_1_shop':
+            # Find the store_id and the week it started selling products
+            broken_id = product_id.split('_')
+            store_id = broken_id[3] + '_' + broken_id[4]
+            product_wm_yr_wk = sell_prices.loc[sell_prices['store_id'] == store_id]['wm_yr_wk'].min()
+
+            # Find the day from which it's sale would have begun [Assumption: Sale started the very first day of the week it has a sale_price]
+            start_day = calendar.index[calendar['wm_yr_wk'] == product_wm_yr_wk][0] # The day number = the row number
+
+            # Get the correct data for a single product
+            if choice1 == 'train':
+                data = train_sales[train_sales['store_id'] == store_id].iloc[ : , start_day + 5 : self.train_val_split + 6] ## + 5 and 6 are present to shift the indices appropriately since the day columns start from the 6th column
+            
+            elif choice1 == 'validation':
+                data = train_sales[train_sales['store_id'] == store_id].iloc[ : , start_day + 5 : ] ## + 5 is present to shift the indices appropriately since the day columns start from the 6th column
+            
+            elif choice1 == 'test':
+                data = test_sales[test_sales['store_id'] == store_id].iloc[ : , start_day + 5 : ] ## + 5 is present to shift the indices appropriately since the day columns start from the 6th column
+            
+
+        elif choice2 == 'one_prod_in_all_shops':
+            # Find the product_id and the week this product's sale started in any of the stores.
+            broken_id = product_id.split('_')
+            item_id = broken_id[0] + '_' + broken_id[1] + '_' + broken_id[2]
+            product_wm_yr_wk = sell_prices.loc[sell_prices['item_id'] == item_id]['wm_yr_wk'].min()
+
+            # Find the day from which it's sale would have begun [Assumption: Sale started the very first day of the week it has a sale_price]
+            start_day = calendar.index[calendar['wm_yr_wk'] == product_wm_yr_wk][0] # The day number = the row number
+
+            # Get the correct data for a single product
+            if choice1 == 'train':
+                data = train_sales[train_sales['item_id'] == item_id].iloc[ : , start_day + 5 : self.train_val_split + 6] ## + 5 and 6 are present to shift the indices appropriately since the day columns start from the 6th column
+            
+            if choice1 == 'validation':
+                data = train_sales[train_sales['item_id'] == item_id].iloc[ : , start_day + 5 : ] ## + 5 is present to shift the indices appropriately since the day columns start from the 6th column
+            
+            if choice1 == 'test':
+                data = test_sales[test_sales['item_id'] == item_id].iloc[ : , start_day + 5 : ] ## + 5 is present to shift the indices appropriately since the day columns start from the 6th column
+            
         return data
         
 
